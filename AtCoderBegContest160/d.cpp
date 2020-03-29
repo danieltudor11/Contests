f52f21b78a2dad942b1f61b9cbfd7606bd2179ef
danieltudor11 <61434618+danieltudor11@users.noreply.github.com>
2020-03-29 03:21:06 -0400

    post contest with hints from editorial/other submissions

--- AtCoderBegContest160/d.cpp
+++ AtCoderBegContest160/d.cpp
@@ -5,14 +5,18 @@ int main(){
 	int n, x, y;
 	cin >> n >> x >> y;
 	
-	vector<int> res;
-	res.resize(n);
-	res[0] = n;
+	vector<int> res (n, 0);
+	
+	for (int i = 1; i < n; i++){
+		for (int j = i+1; j <= n; j++){
+			int temp = min(j - i, abs(x - i) + 1 + abs(y - j));
+			temp = min(temp, abs(y - i) + 1 + abs(x - j));
+			res[temp]++;
+		}
+	}
 
 
-
-
-	for (int i = 0; i < n; i++){
+	for (int i = 1; i < n; i++){
 		cout << res[i] << endl;
 	}
 
