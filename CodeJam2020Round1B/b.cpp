17b7340bd088052d0b032c85621fb4c22d405713
danieltudor11 <61434618+danieltudor11@users.noreply.github.com>
2020-04-19 00:57:07 -0400

    fixed kickstart b names

--- CodeJam2020Round1B/b.cpp
+++ CodeJam2020Round1B/b.cpp
@@ -9,37 +9,31 @@ typedef pair<int, int> pii;
 #define rep(i,a,b) for (int i = a; i < b; i++)
 
 
+void solve(){
+
+
+
+
+}
+
+
 int main(){
 	int t;
 	cin >> t;
 	
-	vvi v;
 	
-	rep(i,1,t+1){
-		int n;
-		cin >> n;
+	//get input
+	rep(i,0,t){
 		
-		vi v1;
 		
-		rep(i,0,n){
-			int temp;
-			cin >> temp;
-			v1.pb(temp);
-		}
-		v.pb(v1);	
 	}
-	
-	
+
+	//produce output
 	rep(i,0,t){
 		cout << "Case #" << i+1 << ": ";
-		int count = 0;
-		rep(j,1,(int)v[i].size()-1){
-			if (v[i][j] > v[i][j-1] && v[i][j] > v[i][j+1]){
-				count++;
-			}
-		}
-		cout << count << endl;
+		solve();
+		cout << endl;
 	}
+	
 	return 0;
 }
-
