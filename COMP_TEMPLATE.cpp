c4f0fbdd04a16fb9edee8d07a83b639b147cac08
Daniel Tudor <61434618+danieltudor11@users.noreply.github.com>
2024-02-07 14:17:34 -0500

    Added a template function for printing vectors

--- COMP_TEMPLATE.cpp
+++ COMP_TEMPLATE.cpp
@@ -1,17 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<string> vs;
-typedef vector<vector<int> > vvi;
+typedef vector<vector<int>> vvi;
 typedef pair<int, int> pii;
 #define pb push_back
-#define rep(i,a,b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
 
-int main(){
-	cout << "hello";
-	
+template <typename T>
+void printVector(const T &t)
+{
+	copy(t.cbegin(), t.cend(), ostream_iterator<typename T::value_type>(cout, ", "));
+}
+
+void solve()
+{
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	rep(i, 1, t + 1)
+	{
+		solve();
+		cout << endl;
+	}
 
 	return 0;
 }
